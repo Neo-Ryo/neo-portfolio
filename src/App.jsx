e1aaa5f35dc46660bb1241b39983c95e7733ac5c
Neo-Ryo <marco.sch4064@gmail.com>
2021-05-03 19:43:32 +0200

    page to 0

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 export const SetMyRender = createContext(0);
 
 function App() {
-  const [page, setPage] = useState(4);
+  const [page, setPage] = useState(0);
   const [previousPage, setPreviousPage] = useState(0);
 
   const switchRendering = (key) => {
